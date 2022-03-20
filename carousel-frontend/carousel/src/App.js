d081c290621c39673191895c2412ed880fa67f45
udeshi-perera <budeshimperera@gmail.com>
2022-03-20 22:16:40 +0530

    initial commit

--- carousel-frontend/carousel/src/App.js
+++ carousel-frontend/carousel/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import SlideImage from './components/SlideImage';
+import { DataFile } from './components/DataFile';
 
 function App() {
-  return <SlideImage/>
+  return <SlideImage slides={DataFile}/>
 }
 
 export default App;
--- carousel-frontend/carousel/src/components/SlideImage.js
+++ carousel-frontend/carousel/src/components/SlideImage.js
@@ -1,15 +1,41 @@
 import React,{useState} from 'react'
 import { DataFile } from './DataFile'
+import {FaArrowAltCircleRight,FaArrowAltCircleLeft} from 'react-icons/fa'
 
-function SlideImage() {
-    const [currImg, setCurrImg] = useState(0);
-    const length = slide.length
+const SlideImage=({slides})=> {
+    const [current, setCurrent] = useState(0);
+    const length = slides.length
+    console.log(length)
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+      };
+    
+      const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+      };
+      console.log(current)
+    
+      if (!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+      }
   return (
-      <>
-      {DataFile.map((slide,index)=>{
-          return <img src ={slide.image} alt="image"></img>
-      })}
-      </>
+    <section className='slider'>
+    <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
+    <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
+    {DataFile.map((slide, index) => {
+      return (
+        <div
+          className={index === current ? 'slide active' : 'slide'}
+          key={index}
+        >
+          {index === current && (
+            <img src={slide.image} alt='travel image' className='image' />
+          )}
+        </div>
+      );
+    })}
+  </section>
   )
 }
 
